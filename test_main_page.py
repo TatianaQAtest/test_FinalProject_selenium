e3129a3d1f3c9d57e9c2fe3d6801c4f10ea468ba
TatianaN <wholeriverinfo@gmail.com>
2025-05-11 12:47:30 +0300

    Added locators.py, login_page.py, changed main_page.py (task 4.2.8)

--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,8 @@
 from .pages.main_page import MainPage
 
 # Запуск теста: pytest -v --tb=line --language=en test_main_page.py 
-# (если Bash открыт в директории, где сам файл test_main_page.py или указать полный путь к нему)
 
 def test_guest_can_go_to_login_page(browser):
-# Тест_гость может перейти по ссылке логина (корректный CSS селектор ссылки в main_page.py):
-# 1.Открыть главную страницу 
-# 2.Проверить, что есть ссылка, которая ведет на логин
  
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
@@ -14,8 +10,6 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()         # выполняем метод страницы — переходим на страницу логина
 
 def test_guest_should_see_login_link(browser):
-# Тест_гость должен видеть ссылку логина (некорректный CSS селектор ссылки в main_page.py)
-# Хотим получить ошибку и адекватное сообщение об ошибке в консоле
 
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
