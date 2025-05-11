9a66e559528ba0b70c5d821e42326328924020e6
TatianaN <wholeriverinfo@gmail.com>
2025-05-11 15:53:27 +0300

    Changed test_product_page.py (task 4.3.4)

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,10 @@
-
+import pytest
 from .pages.product_page import ProductPage
 
+# Запуск теста: pytest -v --tb=line --language=en test_main_page.py
+# Маркировка теста для его пропуска @pytest.mark.skip
+
+@pytest.mark.skip
 def test_guest_can_add_product_to_basket(browser):
     # Задание: добавление в корзину со страницы товара
     # 1. Открываем страницу товара
@@ -17,3 +21,17 @@ def test_guest_can_add_product_to_basket(browser):
     product_page.add_to_basket()             # метод - добавить товар в корзину   
     product_page.solve_quiz_and_get_code()   # метод для получения проверочного кода (в base_page)
     product_page.should_be_add_to_basket()   # метод - проверка,что товар добавлен в корзину (название товара и его цена совпадает)(в product_page)
+
+
+@pytest.mark.parametrize('link_add', ["offer0", "offer1", "offer2", "offer3", "offer4", "offer5", "offer6", "offer7", "offer8", "offer9"])
+def test_guest_can_add_product_to_basket(browser, link_add):
+#Задание: независимость контента, ищем баг (c параметризацией)
+
+    okay_link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo={link_add}"
+
+    product_page = ProductPage(browser, okay_link)
+    product_page.open()   
+ 
+    product_page.add_to_basket()   
+    product_page.solve_quiz_and_get_code()
+    product_page.should_be_add_to_basket()
