5ec067cfcd1e997463f8d2e0ea8eb1511c0dd8f7
TatianaN <wholeriverinfo@gmail.com>
2025-05-11 18:23:51 +0300

    Added test_guest_can_go_to_login_page_from_product_page (task 4.3.8)

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,10 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import math
+from .locators import BasePageLocators
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
@@ -18,8 +22,7 @@ class BasePage():
             return False
         return True
 
-    def solve_quiz_and_get_code(self):
-    # Считаем математическое выражение - метод для получения проверочного кода
+    def solve_quiz_and_get_code(self):    
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
         answer = str(math.log(abs((12 * math.sin(float(x))))))
@@ -32,4 +35,34 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def is_not_element_present(self, how, what, timeout=4):
+    # Этот метод проверяет, что элемент не появляется на странице в течение заданного времени
+    # Упадет, как только увидит искомый элемент. Не появился: успех, тест зеленый.
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False  
+
+    def is_disappeared(self, how, what, timeout=4):
+    # Этот метод проверяет,что элемент исчезает со страницы(используем явное ожидание и  функцию until_not)
+    # Будет ждать до тех пор, пока элемент не исчезнет
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
+        return True
+
+# Перенесли методы из main_page.py
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        # В примере из 4.3 шаг 6 "Плюсы наследования: пример" в параметрах стоит -  LOGIN_LINK_INVALID и тогда тест не проходит.
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
         
--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,10 @@
 import pytest
+import time
 from .pages.product_page import ProductPage
 
 # Запуск теста: pytest -v --tb=line --language=en test_main_page.py
 # Маркировка теста для его пропуска @pytest.mark.skip
+# Маркировка упавших тестов @pytest.mark.xfail
 
 @pytest.mark.skip
 def test_guest_can_add_product_to_basket(browser):
@@ -23,6 +25,7 @@ def test_guest_can_add_product_to_basket(browser):
     product_page.should_be_add_to_basket()   # метод - проверка,что товар добавлен в корзину (название товара и его цена совпадает)(в product_page)
 
 
+@pytest.mark.skip
 @pytest.mark.parametrize('link_add', ["offer0", "offer1", "offer2", "offer3", "offer4", "offer5", "offer6", pytest.param("offer7", marks=pytest.mark.xfail), "offer8", "offer9"])
 def test_guest_can_add_product_to_basket(browser, link_add):
 #Задание: независимость контента, ищем баг (c параметризацией)
@@ -36,3 +39,61 @@ def test_guest_can_add_product_to_basket(browser, link_add):
     product_page.add_to_basket()   
     product_page.solve_quiz_and_get_code()
     product_page.should_be_add_to_basket()
+
+#Задание: отрицательные проверки
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    #Проверяем (с помощью is_not_element_present), что нет сообщения об успехе после добавления товара в корзину
+    #1. Открываем страницу товара 
+    #2. Добавляем товар в корзину 
+    #3. Проверяем, что нет сообщения об успехе.
+   
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+            
+    product_page.add_to_basket()
+    time.sleep(1)
+    product_page.should_not_be_success_message()
+
+@pytest.mark.skip
+def test_guest_cant_see_success_message(browser):  
+    #Проверяем (с помощью is_not_element_present), что нет сообщения об успехе после того, как открыли страницу товара
+    #1. Открываем страницу товара 
+    #2. Проверяем, что нет сообщения об успехе
+
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open() 
+    
+    time.sleep(1)   
+    product_page.should_not_be_success_message()
+
+@pytest.mark.xfail    
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    #Проверяем (с помощью is_disappeared), что нет сообщения об успехе после добавления товара в корзину 
+    #1. Открываем страницу товара
+    #2. Добавляем товар в корзину
+    #3. Проверяем, что нет сообщения об успех.
+
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open() 
+          
+    product_page.add_to_basket()
+    time.sleep(1)
+    product_page.should_be_success_message() 
+
+def test_guest_should_see_login_link_on_product_page(browser):
+#Тест: гость видит ссылку login_link на странице продукта"
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()        
+    page.should_be_login_link()   
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+#Тест: гость может перейти на страницу логина со страницы продукта
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()    
+    page.go_to_login_page()
