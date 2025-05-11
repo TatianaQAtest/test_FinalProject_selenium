30dbffdb53a730ccd9624ec9cb332324b453a0b1
TatianaN <wholeriverinfo@gmail.com>
2025-05-11 14:20:39 +0300

    Сhanged in test_main_page: to go to another page (task 4.2.9)

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,15 +1,17 @@
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 class MainPage(BasePage): 
 # MainPage наследник класса BasePage (в скобках указан класс-предок)
 # Т.о. класс MainPage будет иметь доступ ко всем атрибутам и методам своего класса-предка
 
     def go_to_login_page(self):
+    # метод, который осуществляет переход к странице логина
+
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        #символ *, он указывает на то, что мы передали именно пару(этот кортеж нужно распаковать)
-        login_link.click()
+        # символ *, он указывает на то, что мы передали именно пару(этот кортеж нужно распаковать)
+        login_link.click()        
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 # Запуск теста: pytest -v --tb=line --language=en test_main_page.py 
 
@@ -6,12 +7,12 @@ def test_guest_can_go_to_login_page(browser):
  
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                     # открываем страницу
-    page.go_to_login_page()         # выполняем метод страницы — переходим на страницу логина
+    page.open()                     # открываем страницу  
+    page.go_to_login_page()         # переход к странице логина
+    login_page = LoginPage(browser, browser.current_url) # получение текущего url-страницы (используя свойство  Webdriver) и сохранение её в переменную. 
+    login_page.should_be_login_page() # проверка на наличие элемента (в main_page с assert)
 
-def test_guest_should_see_login_link(browser):
+    
 
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+
+    
